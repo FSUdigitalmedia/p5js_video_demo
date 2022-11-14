7458423b098e5ecb77109390fbba415dd6f9e035
Rob Duarte <me@robduarte.com>
2022-11-14 18:46:15 -0500

    first commit, before making demo video

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+let vid;
+let isPlaying = true;
+
 function setup() {
+  // noCanvas();
   createCanvas(400, 400);
+
+  vid = createVideo(
+    ['assets/bunny.webm'],
+    vidLoad
+  );
+  vid.onended(vidDone);
+  vid.size(900, AUTO);
+  vid.position(100,0);
+
 }
 
 function draw() {
-  background(125);
+  // vid.position(mouseX,mouseY);
+  background(120);
+
+  text(isPlaying,0,20);
+}
+
+function vidDone() {
+  isPlaying = false;
+}
+
+function mouseClicked() {
+  //vid.pause();
+  if (isPlaying) {
+    vid.stop();
+    isPlaying = false;
+  } else {
+    vid.play();
+    isPlaying = true;
+  }
+}
+
+// This function is called when the video loads
+function vidLoad() {
+  // vid.loop();
+  vid.volume(0);
+  vid.play();
 }
